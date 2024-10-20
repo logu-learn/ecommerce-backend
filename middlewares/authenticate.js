44b9c53eba91d68ee9d85dc6419c3eeacc34e05d
logu-learn <nsblogeshwaran@gmail.com>
2024-10-20 18:02:29 +0530

    Payment Feature

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,26 +1,40 @@
 const catchAsyncError = require("./catchAsyncError");
 const ErrorHandler = require("../utils/errorHandler");
 const User = require("../models/userModel");
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 
-exports.isAuthenticatedUser = catchAsyncError(async(req,res,next)=>{
-    const {token} = req.cookies;
-    if(!token){
-        return next(new ErrorHandler("Login first to handle this resource",401))
+// Middleware to check if the user is authenticated
+exports.isAuthenticatedUser = catchAsyncError(async (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    const token = authHeader.split(' ')[1];
+    if (!token) {
+        return next(new ErrorHandler("Please log in to access this resource", 401));
     }
 
-    const decoded = jwt.verify(token,process.env.JWT_SECRET)
-    console.log("decoded:",decoded);
-    
-    req.user = await User.findById(decoded.id)
-    next()
-})
+    try {
+        // Verifying the token and decoding it
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        console.log("decoded:", decoded);
 
-exports.authorizeRoles = (...roles)=>{
-    return (req,res,next) =>{
-        if(!roles.includes(req.user.role)){
-            return next(new ErrorHandler(`Role ${req.user.role} is not allowed`,401))
+        // Fetching the user based on the decoded token id
+        req.user = await User.findById(decoded.id);
+        if (!req.user) {
+            return next(new ErrorHandler("User not found", 404));
         }
-        next()
+
+        next(); // Continue if authenticated
+    } catch (error) {
+        return next(new ErrorHandler("Invalid or expired token", 401));
     }
-}
+});
+
+// Middleware to authorize specific roles
+exports.authorizeRoles = (...roles) => {
+    return (req, res, next) => {
+        if (!roles.includes(req.user?.role)) {
+            return next(new ErrorHandler(`Role '${req.user.role}' is not authorized to access this resource`, 403));
+        }
+        next(); // Continue if role is authorized
+    };
+};
